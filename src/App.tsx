0dc87f0a940a3343e93e0138c60edb30888bb080
ljk4268 <ljk4268@kakao.com>
2022-10-24 18:02:51 +0900

    React + TypeScript 사용할 때 알아야할 점 2 : Redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import {RootState, increment, decrement, incrementByAmount} from './index';
+import {Dispatch} from 'redux';
 
 //JSX 표현하는 타입이 있음. JSX.Element
 let box :JSX.Element = <div></div>
 
 
 function App() {
-  // useState 타입지정 - 타입스크립트가 자동으로 타입을 지정해준다. 
-  // 가끔 string | number을 타입으로 지정하고 싶을 땐?
-  let [user, setUser] = useState<string | number>('kim');
+  const 꺼내온거 = useSelector( (state : RootState) => state );
+  const dispatch = useDispatch();
 
   return (
-    <div>
-      <h4>안녕하십니까</h4>
-      {/* component props 타입지정 */}
-      <Profile name="철수" age="20"></Profile>
+    <div className='App'>
+      {꺼내온거.counter1.count}
+      <button onClick={()=>{dispatch(increment())}}>버튼</button>
     </div>
   );
 }
 
-// component 만들 때 타입지정
-// 함수타입지정은 파라미터 & return 값
 
-function Profile(props :{name :string, age :string}) :JSX.Element{
-  return(
-    <div>{props.name}프로필입니다.</div>
-  )
-}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,48 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createSlice, configureStore, PayloadAction } from '@reduxjs/toolkit';
+
+
+const 초기값 :{count :number, user :string}= { count: 0, user : 'kim' };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState : 초기값,
+  reducers: {
+    increment (state){
+      state.count += 1
+    },
+    decrement (state){
+      state.count -= 1
+    },
+    incrementByAmount (state, action :PayloadAction<number>){
+      state.count += action.payload
+    }
+  }
+})
+
+let store = configureStore({
+  reducer: {
+    counter1 : counterSlice.reducer
+  }
+})
+
+// store의 타입 미리 export 해두기 
+export type RootState = ReturnType<typeof store.getState>
+
+//수정방법 만든거 export
+export let {increment, decrement, incrementByAmount} = counterSlice.actions
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
