6587c820a3f31d869ca4491ce7006a324baba53a
ljk4268 <ljk4268@kakao.com>
2022-10-24 12:45:08 +0900

    React + TypeScript 사용할 때 알아야할 점

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+//JSX 표현하는 타입이 있음. JSX.Element
+let box :JSX.Element = <div></div>
+
+
 function App() {
+  // useState 타입지정 - 타입스크립트가 자동으로 타입을 지정해준다. 
+  // 가끔 string | number을 타입으로 지정하고 싶을 땐?
+  let [user, setUser] = useState<string | number>('kim');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>안녕하십니까</h4>
+      {/* component props 타입지정 */}
+      <Profile name="철수" age="20"></Profile>
     </div>
   );
+}
+
+// component 만들 때 타입지정
+// 함수타입지정은 파라미터 & return 값
+
+function Profile(props :{name :string, age :string}) :JSX.Element{
+  return(
+    <div>{props.name}프로필입니다.</div>
+  )
 }
 
 export default App;
